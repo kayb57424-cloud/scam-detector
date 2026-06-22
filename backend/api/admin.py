3a5cfb4f2c674199ebde45aff51630b28d24e11b
kaybee117 <monahengkaybee8@gmail.com>
2026-06-22 23:40:57 +0200

    Register Report model in admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Report
 
-# Register your models here.
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "phone_number",
+        "phone_country",
+        "source_type",
+        "result",
+        "reporter_region",
+        "created_at"
+    )
+
+    search_fields = (
+        "phone_number",
+        "message",
+        "reporter_region"
+    )
+
+    list_filter = (
+        "result",
+        "source_type",
+        "reporter_region",
+        "phone_country"
+    )
+
+    ordering = ("-created_at",)
